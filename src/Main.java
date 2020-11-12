6c3d0bc44764c441e23ba50f3c465ec24d0aec8f
Denis Sitek <ugfan13@gmail.com>
2020-11-12 17:55:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int transfer = 500_000_000;
+        int totalaccount = account + transfer;
+        System.out.println(totalaccount);
+    }
+}
